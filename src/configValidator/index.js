240f5c596690303e506c7046bf728d91d77ceeb0
Jonathan Werner <jwerner@posteo.de>
2016-03-25 14:28:01 +0100

    refactor(core): Change API of configValidator
    It does not have console.logging side effects anymore, but just exposes validation messages as data and formatted strings

--- src/configValidator/index.js
+++ src/configValidator/index.js
@@ -1,21 +1,25 @@
+import {groupBy} from 'lodash'
 import checkConfig from '../checkConfig'
 import uncoveredPaths from '../uncoveredPaths'
-import messageFormatter from '../messageFormatter'
+import formatMessagesAsString from '../formatMessagesAsString'
 
 export default configValidator
 
 function configValidator(configName, config, validators) {
-  const uncoveredFields = getWarningsForUncoveredFields(config, validators)
-  const results = checkConfig(config, validators)
-  const totalResults = uncoveredFields.concat(results)
-  if (totalResults.length) {
-    const {error, warning} = messageFormatter(totalResults)
-    log(configName, error)
-    log(configName, warning)
+  const uncoveredFieldsMessages = getWarningMessagesForUncoveredFields(config, validators)
+  const validationMessages = checkConfig(config, validators)
+  const messages = [...uncoveredFieldsMessages, ...validationMessages]
+  const {warning: warnings = [], error: errors = []} = groupBy(messages, 'type')
+
+  return {
+    errors,
+    warnings,
+    warningMessage: formatMessagesAsString(warnings),
+    errorMessage: formatMessagesAsString(errors),
   }
 }
 
-function getWarningsForUncoveredFields(config, validators) {
+function getWarningMessagesForUncoveredFields(config, validators) {
   return uncoveredPaths(config, validators)
     .map(uncoveredPath => {
       return {
@@ -24,10 +28,4 @@ function getWarningsForUncoveredFields(config, validators) {
         type: 'warning',
       }
     })
-}
-
-function log(configName, message) {
-  if (message) {
-    console.log(`${configName}:\n${message}`) // eslint-disable-line no-console
-  }
 }
--- src/configValidator/index.test.js
+++ src/configValidator/index.test.js
@@ -1,38 +1,73 @@
 /* eslint no-console:0 */
 import test from 'ava'
 import sinon from 'sinon'
+
 import configValidator from './index'
 
-let originalConsole
+const EMPTY_RESULT = {
+  errors: [],
+  warnings: [],
+  warningMessage: undefined,
+  errorMessage: undefined,
+}
 
-test('no log with no error or messages', t => {
-  setup()
-  configValidator('Webpack Config', {}, [])
-  t.false(console.log.called)
-  cleanUp()
+test('No errors or warnings result in an return object with sensible "empty" values', t => {
+  const result = configValidator('Webpack Config', {}, [])
+  t.same(result, EMPTY_RESULT)
 })
 
-test('logs error messages', t => {
-  setup()
+test('Errors are returned in the `errors` property of the return object ' +
+     'and as formatted string under `errorMessage`', t => {
   const config = {bar: 'baz'}
   const validators = [{key: 'bar', validate: () => 'error'}]
-  configValidator('Webpack Config', config, validators)
-  t.true(console.log.calledOnce)
-  t.true(console.log.calledWithMatch(/Webpack Config[\s\S]*?bar[\s\S]*?error/))
-  cleanUp()
+  const result = configValidator('Webpack Config', config, validators)
+  t.same(result.errors, [
+    {
+      key: 'bar',
+      message: 'error',
+      value: 'baz',
+      validator: validators[0],
+      type: 'error',
+    },
+  ])
+  t.ok(result.errorMessage) // Test implementation details of this in `formatMessagesAsString` tests
 })
 
-test('unknown fields', t => {
-  setup()
+test('Warnings are returned in the `warnings` property of the return object ' +
+     'and as formatted string under `warningMessage`', t => {
+  const config = {bar: 'baz'}
+  const validators = [{key: 'bar', validate: () => ({warning: 'warning'})}]
+  const result = configValidator('Webpack Config', config, validators)
+  t.same(result.warnings, [
+    {
+      key: 'bar',
+      message: 'warning',
+      value: 'baz',
+      validator: validators[0],
+      type: 'warning',
+    },
+  ])
+  t.ok(result.warningMessage) // Test implementation details of this in `formatMessagesAsString` tests
+})
+
+test('Unknown fields result in a warning', t => {
   const config = {bar: 'baz', cat: 'bag'}
   const validators = [{key: 'bar', validate: () => 'error'}]
-  configValidator('Webpack Config', config, validators)
-  t.true(console.log.calledWithMatch(/Webpack Config[\s\S]*?bar[\s\S]*?error/))
-  cleanUp()
+  const result = configValidator('Webpack Config', config, validators)
+
+  t.ok(result.errors.length === validators.length)
+  t.ok(result.errorMessage)
+
+  t.ok(result.warnings.length === 1)
+  t.same(result.warnings[0], {
+    key: 'cat',
+    message: 'Unknown key',
+    type: 'warning',
+  })
+  t.ok(result.warningMessage)
 })
 
-test('nested fields should not trigger', t => {
-  setup()
+test('Nested fields should not trigger warnings if they validate', t => {
   const config = {
     foo: {
       bar: true,
@@ -43,13 +78,11 @@ test('nested fields should not trigger', t => {
     {key: 'foo.bar', validate() {}},
     {key: 'foo.baz', validate() {}},
   ]
-  configValidator('Webpack Config', config, validators)
-  t.false(console.log.called)
-  cleanUp()
+  const result = configValidator('Webpack Config', config, validators)
+  t.same(result, EMPTY_RESULT)
 })
 
-test('null fields should not trigger', t => {
-  setup()
+test('Null fields should not trigger warnings if they validate', t => {
   const config = {
     foo: null,
     help: 'me',
@@ -58,13 +91,11 @@ test('null fields should not trigger', t => {
     {key: 'foo', validate() {}},
     {key: 'help', validate() {}},
   ]
-  configValidator('Webpack Config', config, validators)
-  t.false(console.log.called)
-  cleanUp()
+  const result = configValidator('Webpack Config', config, validators)
+  t.same(result, EMPTY_RESULT)
 })
 
-test('arrays fields should not have nested checks', t => {
-  setup()
+test('Array fields should not have nested checks', t => {
   const config = {
     foo: null,
     help: ['me', 'you', 'them'],
@@ -73,13 +104,11 @@ test('arrays fields should not have nested checks', t => {
     {key: 'foo', validate() {}},
     {key: 'help', validate() {}},
   ]
-  configValidator('Webpack Config', config, validators)
-  t.false(console.log.called)
-  cleanUp()
+  const result = configValidator('Webpack Config', config, validators)
+  t.same(result, EMPTY_RESULT)
 })
 
-test('nested fields can trigger though when not validated', t => {
-  setup()
+test('Nested fields trigger when not validated', t => {
   const config = {
     foo: {
       bar: true,
@@ -89,13 +118,17 @@ test('nested fields can trigger though when not validated', t => {
   const validators = [
     {key: 'foo.bar', validate() {}},
   ]
-  configValidator('Webpack Config', config, validators)
-  t.true(console.log.calledOnce)
-  cleanUp()
+  const result = configValidator('Webpack Config', config, validators)
+
+  t.ok(result.warnings.length === 1)
+  t.same(result.warnings[0], {
+    key: 'foo.baz',
+    message: 'Unknown key',
+    type: 'warning',
+  })
 })
 
-test('nesteder fields can trigger though when not validated', t => {
-  setup()
+test('Deeply nested fields trigger when not validated', t => {
   const config = {
     foo: {
       bar: {
@@ -107,26 +140,24 @@ test('nesteder fields can trigger though when not validated', t => {
   const validators = [
     {key: 'foo.bar.cat', validate: () => 'error'},
   ]
-  configValidator('Webpack Config', config, validators)
-  t.true(console.log.calledTwice)
-  cleanUp()
+  const result = configValidator('Webpack Config', config, validators)
+  t.ok(result.errors.length === 1)
+  t.same(result.errors[0], {
+    key: 'foo.bar.cat',
+    message: 'error',
+    type: 'error',
+    validator: validators[0],
+    value: 'sink',
+  })
 })
 
-test('logs warning messages', t => {
-  setup()
+test('Does not log anything, that\'s left to the API consumer', t => {
+  const consoleLogSpy = sinon.spy(console, 'log')
+
   const config = {bar: 'baz'}
   const validators = [{key: 'bar', validate: () => ({warning: 'warning'})}]
   configValidator('Webpack Config', config, validators)
-  t.true(console.log.calledOnce)
-  t.true(console.log.calledWithMatch(/Webpack Config[\s\S]*?bar[\s\S]*?warning/))
-  cleanUp()
+  t.true(consoleLogSpy.callCount === 0)
+
+  consoleLogSpy.restore()
 })
-
-function setup() {
-  originalConsole = console.log
-  console.log = sinon.spy()
-}
-
-function cleanUp() {
-  console.log = originalConsole
-}
